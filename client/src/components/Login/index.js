6feb2ad40625f73059cab1cffba36ed2a4a94bc7
Farah <farah9hias@gmail.com>
2022-08-20 16:12:08 -0700

    updated app icon

---
+++ client/src/components/Login/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Login () {
+    return (
+        <header>
+            <h1>Personal Finance Blog</h1>
+        </header>
+    )
+}
+
+export default Nav;
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -4,6 +4,7 @@ function Nav () {
     return (
         <header>
             <h1>Personal Finance Blog</h1>
+            <p>Welcome to the blog of <span class="w3-tag">FINANCE</span></p>
         </header>
     )
 }
